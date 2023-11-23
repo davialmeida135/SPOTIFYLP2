b711226fab2770d8d403392d825fab18a225f4ba
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2023-11-23 12:34:01 -0300

    SwitchScene funcioanndo

--- SpotifyJava/src/com/spotify/control/MenuController.java
+++ SpotifyJava/src/com/spotify/control/MenuController.java
@@ -5,23 +5,24 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
+
+import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import com.spotify.view.MenuView;
-import javafx.scene.layout.AnchorPane;
+
+
 
 public class MenuController {
-
 	@FXML
-	private AnchorPane MenuAnchorPane;
+    private ListView<String> ListaPlaylists;
+	@FXML
+    private ImageView playButton;
+
 	
+	@FXML
+    void playClicked(ActionEvent event) {
+		System.out.println("APERTA O PLAY");
+		//ListaPlaylists.getItems().add("oi");
+    }
+
+    
 }
