7044b38c7bc76eaac1a29e27a6013197b0ce5734
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2023-11-28 14:23:03 -0300

    Preparacao musicaDAO

--- SpotifyJava/src/com/spotify/dao/MusicaDAO.java
+++ SpotifyJava/src/com/spotify/dao/MusicaDAO.java
@@ -8,23 +8,33 @@ import com.spotify.data.DataBase;
 public class MusicaDAO {
 	
 	public static void novaMusica(String nome, String path,Connection conn) {
+		String sql = "INSERT INTO musicas(nome, path)"
+	 			+ "VALUES(?,?)";
+	}
+	
+	
+	//recebe o path de uma pasta e adiciona todas as musicas
+	public static void novoDiretorio(String path,int userId,Connection conn) {
 		
 	}
 	
-	public static void novoDiretorio(String path,Connection conn) {
+	public static void removerMusica(String nome,int userId,Connection conn) {
+		String sql = "SELECT musicas.id FROM musicas "
+				+ "WHERE musicas.nome = ?";
 		
-	}
-	
-	public static void removerMusica(String nome,Connection conn) {
 		
 	}
 	
 	public static int  getMusicaId(String nome, int userId,Connection conn) {
-		
+		String sql = "SELECT musicas.id FROM musicas "
+				+ "WHERE musicas.nome = ?";
 		return -1;
 	}
 	public static String getMusicaPath(String nome, int userId,Connection conn) {
 		String path = null;
+		String sql = "SELECT musicas.path FROM musicas "
+				+ "WHERE musicas.nome = ?";
+		
 		
 		return path;
 	}
