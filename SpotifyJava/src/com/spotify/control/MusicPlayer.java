91e632ba7f2c8ae82367d1291bedf41422aaab26
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2023-12-05 19:10:48 -0300

    dasdas

--- SpotifyJava/src/com/spotify/control/MusicPlayer.java
+++ SpotifyJava/src/com/spotify/control/MusicPlayer.java
@@ -1,6 +1,11 @@
 package com.spotify.control;
 
 import java.io.File;
+<<<<<<< Updated upstream
+=======
+
+import java.util.ArrayList;
+>>>>>>> Stashed changes
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -10,6 +15,7 @@ import javafx.application.Application;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 //Gerenciar a fila e tocar as musicas
 //Fazer funcao de adicionar musica a fila
@@ -17,8 +23,9 @@ import javafx.stage.Stage;
 public class MusicPlayer extends Application {
 	private static Queue<Musica> fila = new LinkedList<Musica>();
 	private static int isPlaying=0;
+	private Duration pauseTime = new Duration(0.0);
 	
-	private static MediaPlayer currentPlayer;
+	private MediaPlayer currentPlayer;
 	
 	public MusicPlayer() {
 		String q = "./storage/musicas/DreamTeamGanharDragoesNegros.mp3";
@@ -38,29 +45,35 @@ public class MusicPlayer extends Application {
 	}
 	
 	public void pausar() {
-		currentPlayer.pause();
-		isPlaying=0;
+		if(isPlaying==1) {
+			currentPlayer.pause();
+			isPlaying=0;
+			pauseTime = currentPlayer.getCurrentTime();
+			System.out.println(currentPlayer.getCurrentTime());
+		}	
 	}
 	
 	public void playNextMusicFile(Queue<Musica> fila) {
 		  if (fila.isEmpty()||isPlaying==1) {
 		    return ;
 		  }
-		 
-		
-		  isPlaying =1;
+		  
+		  isPlaying = 1;
 		  Media media = new Media(new File(fila.element().getPath()).toURI().toString());
 		  MediaPlayer player = new MediaPlayer(media);
 		  currentPlayer = player;
 		  player.setOnEndOfMedia(() -> {
 		    player.dispose();
 		    isPlaying =0;
+		    pauseTime = Duration.ZERO;
 		    fila.remove();
 		    playNextMusicFile(fila);
 		    
 		  });
+		  System.out.println(pauseTime);
+		  player.seek(pauseTime);
 		  player.play();
-		}
+	}
 	
 	@Override
 	public void start(Stage stage) throws Exception {
